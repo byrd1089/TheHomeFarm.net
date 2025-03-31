77e87ab61723735255f9f76528ad238b290ed6e2
byrd1089 <byrd1089@gmail.com>
2025-03-31 14:38:38 -0400

    added tailwinds

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
-import Todos from './components/Todos.vue'
   
 </script>
 
 <template>
-  <main>
-    <Todos />
-  </main>
+<section class="dark:bg-gray-100 dark:text-gray-800">
+		<div class="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
+			<div class="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+				<h1 class="text-4xl md:text-6xl tracking-tight leading-none font-extrabold text-blue-500">The Home Farm</h1>
+				<p class="mt-4 mb-6 text-lg sm:mb-12">Home of the Young Family Clans.
+					<br class="hidden md:inline lg:hidden">qwerty
+				</p>
+				<!-- <div class="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+					<a rel="noopener noreferrer" href="#" class="px-8 py-3 text-lg font-semibold rounded dark:bg-violet-600 dark:text-gray-50">Suspendisse</a>
+					<a rel="noopener noreferrer" href="#" class="px-8 py-3 text-lg font-semibold border rounded dark:border-gray-800">Malesuada</a>
+				</div> -->
+			</div>
+			<div class="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+				<img src="./assets/home-farm-2.jpg" alt="" class="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+			</div>
+		</div>
+	</section>
 </template>
 
